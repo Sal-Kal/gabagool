36da18c3ee17d1ea9bb5df6358ea6e13aae22272
Sal-Kal <saishkalbag@gmail.com>
2024-12-08 19:15:27 +0530

    Added better base configs for neovim

---
+++ nvim/lua/keymaps/general.lua
@@ -0,0 +1,9 @@
+-- Keymaps
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>fs", "<cmd>w<CR>")
+vim.keymap.set("n", "<space>bk", "<cmd>q!<CR>")
+vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("n", "<C-j>", "<C-d>zz")
+vim.keymap.set("n", "<C-k>", "<C-u>zz")
+vim.keymap.set("v", "<C-j>", "<C-d>zz")
+vim.keymap.set("v", "<C-k>", "<C-u>zz")
