c8f552eea7c6697c29a0ebbb7d20316636e4beca
Sal-Kal <saishkalbag@gmail.com>
2024-12-10 18:46:13 +0530

    Removed mini nvim and added tree sitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,13 @@
 require("configs")
 require("keymaps")
 
+-- Universal Options
+vim.opt.clipboard = "unnamedplus"
+vim.opt.shiftwidth = 2
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Auto Commands
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
