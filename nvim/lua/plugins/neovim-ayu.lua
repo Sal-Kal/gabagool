ee6700cbafe68b6e8fe3366c0697b1b68c305635
Sal-Kal <saishkalbag@gmail.com>
2024-12-22 15:15:37 +0530

    Changed neovim color scheme, updated lazy packages

--- nvim/lua/plugins/neovim-ayu.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "Shatur/neovim-ayu",
-    config = function()
-        require("ayu").setup({
-            mirage = false, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
-        })
-        require("ayu").colorscheme()
-    end,
-}
